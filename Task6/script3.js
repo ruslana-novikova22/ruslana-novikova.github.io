e68c48189a841127d267c46b9d356ad6b3e393f4
ruslana-novikova22 <128227563+ruslana-novikova22@users.noreply.github.com>
2023-03-30 20:59:10 +0300

    Update script3.js

--- Task6/script3.js
+++ Task6/script3.js
@@ -4,7 +4,7 @@
 let namesOfStudents = ["Артем", "Юрій", "Юліана", "Іван", "Мирослава", "Юрій", "Віктор", "Алла", "Марія", "Данило", "Юрій"];
 let count = 0 
 for (let i =0; i<namesOfStudents.length; i++){
-    if(namesOfStudents[i]==="Юрій"){
+    if(namesOfStudents[i]=="Юрій"){
         count++;
     }
 }
