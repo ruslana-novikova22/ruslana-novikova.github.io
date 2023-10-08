bd74d69677b1dc3b031bd48028dfab02e003bffb
ruslana-novikova22 <128227563+ruslana-novikova22@users.noreply.github.com>
2023-10-08 23:57:55 +0300

    Add files via upload

---
+++ Лаб1 Шаблони/Лаб1 Шаблони/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаб1_Шаблони
+{
+    class Users : Meeting
+    {
+        public int id;
+        public string name;
+        public string avatar;
+        public override string ToString()
+        {
+            return base.ToString() + $" id: {id} name: {name} avatar: {avatar} ";
+        }
+    }
+}
