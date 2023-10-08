f9abedacfc70782f7cd454038981090ccb461195
ruslana-novikova22 <128227563+ruslana-novikova22@users.noreply.github.com>
2023-10-08 21:48:59 +0300

    Add files via upload

---
+++ Lab1/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Шаблони_Лаб1
+{
+    class Meeting
+    {
+        public DateOnly date;
+        public string description;
+        public string url;
+        public List<Users> users;
+
+        public override string ToString()
+        {
+            return $"{date}: {description}: {url}: {users.ToString}";
+        }
+    }
+
+}
---
+++ Lab1/EventReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Шаблони_Лаб1
+{
+    abstract class EventReader
+    {
+        public abstract List<Meeting> ReadFromFile(string FileName);
+
+        public static EventReader Create(string Filename)
+        {
+            var parts = Filename.Split('.');
+            if (parts.Length > 1 && parts[parts.Length - 1] == "xml")
+                return new EventXMLReader(); 
+            return null;
+        }
+    }
+}
