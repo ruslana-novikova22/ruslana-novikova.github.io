00b134998f93ba4cb17c057cce01830d1d7deca6
ruslana-novikova22 <128227563+ruslana-novikova22@users.noreply.github.com>
2023-11-02 01:11:32 +0200

    Add files via upload

---
+++ Шаблони проектування/Lab1_Shablony/Lab1_Shablony/Program.cs
@@ -0,0 +1,143 @@
+﻿/*Варіант 4.Розробити клас підсистеми для завантаження даних про онлайн зустріч (дата, опис, URL, список учасників)
+ * та списку всіх користувачів системи (ID, Ім'я, аватар) з файлів в форматі JSON та XML. 
+ * Визначення типу файлу визначається при запуску системи. Передбачити, що дані про кожну зустріч та кожного 
+ * користувача зберігаються в окремих файлах. Файл зустрічі містить імена файлів - учасників цієї зустрічі.*/
+
+using System.Text.Json;
+using System.Xml.Serialization;
+using System.IO;
+using Newtonsoft.Json;
+
+public interface IJsonDataReader
+{
+    Meeting ReadData<Meeting>(string filePath);
+}
+
+public interface IXmlDataReader<User>
+{
+    List<User> ReadData(string filePath);
+}
+public class JsonDataReader : IJsonDataReader
+{
+    public Meeting ReadData<Meeting>(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            string jsonText = File.ReadAllText(filePath);
+            Meeting data = JsonConvert.DeserializeObject<Meeting>(jsonText);
+            return data;
+        }
+        else
+        {
+            throw new FileNotFoundException("File not found");
+        } 
+    }
+}
+
+public class XmlDataReader<User> : IXmlDataReader<User>
+{
+    public List<User> ReadData(string filePath)
+    {
+        List<User> data;
+
+        using (var streamReader = new StreamReader(filePath))
+        {
+            var serializer = new XmlSerializer(typeof(List<User>), new XmlRootAttribute("Users"));
+            data = (List<User>)serializer.Deserialize(streamReader);
+        }
+
+        return data;
+    }
+}
+
+public class DataReaderFactory
+{
+    public static IJsonDataReader CreateJsonDataReader()
+    {
+        return new JsonDataReader();
+    }
+
+    public static IXmlDataReader<User> CreateXmlDataReader()
+    {
+        return new XmlDataReader<User>();
+    }
+}
+public class Meeting
+{
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+    public string Url { get; set; }
+
+    [XmlArray("Participants")]
+    [XmlArrayItem("Participant")]
+    public List<string> Participants { get; set; }
+}
+
+public class User
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Avatar { get; set; }
+}
+public class MeetingLoader
+{
+    private IJsonDataReader jsonDataReader;
+
+    public MeetingLoader(IJsonDataReader jsonDataReader)
+    {
+        this.jsonDataReader = jsonDataReader;
+    }
+
+    public Meeting LoadMeetingData(string filePath)
+    {
+        return jsonDataReader.ReadData<Meeting>(filePath);
+    }
+}
+
+public class UserLoader
+{
+    private IXmlDataReader<User> xmlDataReader;
+
+    public UserLoader(IXmlDataReader<User> xmlDataReader)
+    {
+        this.xmlDataReader = xmlDataReader;
+    }
+
+    public List<User> LoadUserData(string filePath)
+    {
+        return xmlDataReader.ReadData(filePath);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var jsonDataReader = DataReaderFactory.CreateJsonDataReader();
+        var meetingLoader = new MeetingLoader(jsonDataReader);
+
+        var xmlDataReader = DataReaderFactory.CreateXmlDataReader();
+        var userLoader = new UserLoader(xmlDataReader);
+
+        var meeting = meetingLoader.LoadMeetingData("meeting.json");
+        Console.WriteLine("Meeting Date: " + meeting.Date);
+        Console.WriteLine("Meeting Description: " + meeting.Description);
+        Console.WriteLine("Meeting URL: " + meeting.Url);
+        Console.WriteLine("Meeting Participants:");
+        foreach (var participant in meeting.Participants)
+        {
+            Console.WriteLine(participant);
+        }
+
+        List<User> users = userLoader.LoadUserData("user.xml");
+        foreach (var user in users)
+        {
+            Console.WriteLine("User ID: " + user.ID);
+            Console.WriteLine("User Name: " + user.Name);
+            Console.WriteLine("User Avatar: " + user.Avatar);
+            Console.WriteLine();
+        }
+    }
+}
+
+
---
+++ Шаблони проектування/modul1_Shablony/modul1_Shablony/Program.cs
@@ -0,0 +1,144 @@
+﻿/*Об’єкт Рахунок в банку (ПІБ власника, ІПН, сума на рахунку) містить метод
+Оплатити (Призначення, дата, сума), та Поповнити рахунок (сума). Реалізувати
+фінансовий моніторинг за користувачем: при кожній оплаті, сума якої
+перевищує неоподаткований мінімум (17 грн.), надсилати дані до податкової
+інспекції.
+Вказати шаблон, який доцільно використати для розв'язування задачі.*/
+
+//В цій задачі я використала шаблон Декоратор, бо він дозволяє додати новий функціонал до об'єкта без зміни структури коду.
+//Ми створили декоратор для Рахунка у банку і додали до нього функції оплати і поповнення рахунку
+
+using System;
+
+interface IBankAccount
+{
+    void AccountInfo();
+}
+
+class BankAccount : IBankAccount
+{
+    private string Surname;
+    private string IPN;
+    private double Balance;
+
+    public BankAccount(string surname, string ipn, double balance)
+    {
+        Surname= surname;
+        IPN= ipn;
+        Balance = balance;
+    }
+
+    public void AccountInfo()
+    {
+        Console.WriteLine($"Власник: {Surname}, ІПН: {IPN}, Сума на рахунку: {Balance}");
+    }
+}
+
+class PaymentDecorator : IBankAccount
+{
+    private IBankAccount BankAccount;
+    private string Purpose;
+    private DateTime Date;
+    private double Amount;
+
+    public PaymentDecorator(IBankAccount bankAccount, string purpose, DateTime date, double amount)
+    {
+        BankAccount = bankAccount;
+        Purpose = purpose;
+        Date = date;
+        Amount = amount;
+    }
+
+    public void AccountInfo()
+    {
+        Console.WriteLine("Виконано оплату:");
+        Console.WriteLine($"Призначення: {Purpose}");
+        Console.WriteLine($"Дата: {Date}");
+        Console.WriteLine($"Сума: {Amount}");
+        BankAccount.AccountInfo();
+
+        if (Amount > 17.0)
+        {
+            SendToInspection(Amount);
+        }
+    }
+
+    private void SendToInspection(double amount)
+    {
+        Console.WriteLine($"Дані відправлені до податкової інспекції за оплату у розмірі {amount} грн.");
+    }
+}
+
+class DepositDecorator : IBankAccount
+{
+    private IBankAccount BankAccount;
+    private double Deposit;
+
+    public DepositDecorator(IBankAccount bankAccount, double amount)
+    {
+        BankAccount = bankAccount;
+        Deposit = amount;
+    }
+
+    public void AccountInfo()
+    {
+        Console.WriteLine("Поповнено рахунок на суму: " + Deposit);
+        BankAccount.AccountInfo();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть ваше ім'я: ");
+        string surname = Console.ReadLine();
+
+        Console.Write("Введіть ваш ІПН: ");
+        string ipn = Console.ReadLine();
+
+        Console.Write("Введіть початковий баланс рахунку: ");
+        double balance = double.Parse(Console.ReadLine());
+
+        IBankAccount account = new BankAccount(surname, ipn, balance);
+
+        while (true)
+        {
+            Console.WriteLine("\nОберіть опцію:");
+            Console.WriteLine("1. Оплатити");
+            Console.WriteLine("2. Поповнити рахунок");
+            Console.WriteLine("3. Вийти");
+            Console.Write("Ваш вибір: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                Console.Write("Введіть призначення оплати: ");
+                string purpose = Console.ReadLine();
+
+                Console.Write("Введіть дату оплати: ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Введіть суму оплати: ");
+                double amount = double.Parse(Console.ReadLine());
+
+                account = new PaymentDecorator(account, purpose, date, amount);
+            }
+            else if (choice == 2)
+            {
+                Console.Write("Введіть суму поповнення рахунку: ");
+                double depositAmount = double.Parse(Console.ReadLine());
+
+                account = new DepositDecorator(account, depositAmount);
+            }
+            else if (choice == 3)
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("\nЗагальна інформація про рахунок:");
+        account.AccountInfo();
+    }
+}
+
