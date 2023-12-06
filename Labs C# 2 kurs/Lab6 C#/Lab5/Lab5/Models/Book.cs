4c0ae8ab82452f3831e27890a3d5c5bb1234005f
ruslana-novikova22 <128227563+ruslana-novikova22@users.noreply.github.com>
2023-12-07 01:30:33 +0200

    Add files via upload

---
+++ Labs C# 2 kurs/Lab6 C#/Lab5/Lab5/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.Models
+{
+    internal class Book
+    {
+        public int Id;
+        public string Title;
+        public string Author;
+        public int PublicationYear;
+        public string AuthorAdress;
+        public string PublisherAddress;
+        public decimal Price;
+        public string BookstoreFirm;
+        public int PublishingCount;
+    }
+}
