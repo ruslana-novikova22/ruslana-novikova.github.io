6be6be1c7e9b7d828d892cede113fea25cc5640b
ruslana-novikova22 <128227563+ruslana-novikova22@users.noreply.github.com>
2023-12-07 18:06:33 +0200

    Add files via upload

---
+++ Labs C# 2 kurs/Lab6 C#/Lab5/Lab5/Program.cs
@@ -0,0 +1,133 @@
+﻿/*Варіант 4. Створити таблицю бази даних про книги, які були куплені бібліотекою: назва, автор, рік видання,
+адреса автора, адреса видавництва, ціна, книготорговельна фірма.
+
+Використовуючи SqlCommand підготувати програмну оболонку для виконання завдань лабораторної роботи 5. 
+Забезпечити користувачу можливість ввести значення параметрів запиту.
+
+. Cтворити наступні види SQL-запитів:
+a) простий запит на вибірку;
+b) запит на вибірку з використанням спеціальних функцій: LIKE, IS NULL, IN, BETWEEN;
+c) запит зі складним критерієм;
+d) запит з унікальними значеннями;
+e) запит з використанням обчислювального поля;
+f) запит з групуванням по заданому полю, використовуючи умову групування;
+g) запит із сортування по заданому полю в порядку зростання та спадання значень;
+h) запит з використанням дій по модифікації записів.*/
+
+using Lab5.Models;
+using System.Data.SqlClient;
+
+namespace Lab6
+{
+    class Program
+    {
+        public static void Main()
+        {
+            const string Connection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\MegaNotik\Desktop\Універ\Лабораторні роботи C#\Labs C# 2 kurs\Lab6 C#\Lab5\Lab5\SA2_Novikova.mdf;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(Connection))
+            {
+                connection.Open();
+                var rep = new BookRepository(connection);
+
+                //Простий запит на вибірку
+                Console.Write("Enter the name of author: ");
+                string authorName = Console.ReadLine();
+                var books = rep.GetBooks(authorName);
+                Console.WriteLine("Books by author:");
+                foreach (Book book in books)
+                {
+                    Console.WriteLine($"{book.Title}-{book.Price}-{book.BookstoreFirm}");
+                }
+
+                //Запит на вибірку з використанням спеціальних функцій: LIKE, IS NULL, IN, BETWEEN;
+                Console.Write("Enter minimal price: ");
+                decimal minPrice = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Enter maximum price: ");
+                decimal maxPrice = Convert.ToDecimal(Console.ReadLine());
+                books = rep.GetPrices(minPrice, maxPrice);
+                if (books.Count == 0)
+                {
+                    Console.WriteLine("No books found in the specified price range.");
+                }
+                else
+                {
+                    Console.WriteLine("List of books:");
+                    foreach (Book book in books)
+                    {
+                        Console.WriteLine($"{book.Title}-{book.Author}-{book.PublicationYear}-{book.AuthorAdress}-{book.PublisherAddress}-{book.Price}-{book.BookstoreFirm}");
+                    }
+                }
+
+                //Запит зі складним критерієм;
+                Console.Write("Enter year: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter bookstore firm: ");
+                string firm = Convert.ToString(Console.ReadLine());
+                var books1 = rep.GetBooks1(year, firm);
+                Console.WriteLine("List of books:");
+                foreach (Book book in books)
+                {
+                    Console.WriteLine($"{book.Title}-{book.Author}- {book.Price}");
+                }
+
+                //Запит з унікальними значеннями;
+                Console.WriteLine("The List of authors:");
+                string author = Console.ReadLine();
+                var authors = rep.GetAuthors(author);
+                foreach (var au in authors)
+                {
+                    Console.WriteLine(au.Author);
+                }
+
+                //Запит з використанням обчислювального поля               
+                Console.Write("Enter the price multiplier: ");
+                string multiplier = Console.ReadLine();
+                var prices = rep.GetNewPrice(multiplier);
+                Console.WriteLine("New prices:");
+                foreach (var p in prices)
+                {
+                    Console.WriteLine($"{p.Title} - {p.Price}");
+                }
+
+                //Запит з групуванням по заданому полю, використовуючи умову групування;
+                Console.Write("Enter the firm: ");
+                string firm1 = Console.ReadLine();
+                int count = 0;
+
+                var counts = rep.GetCount(firm1);
+                foreach (var item in counts)
+                {
+                    count++;
+                    Console.WriteLine($"{item.BookstoreFirm}: {item.PublishingCount}");
+                }
+
+                if (count == 0)
+                {
+                    Console.WriteLine("No information found for the specified firm.");
+                }
+
+                //Запит із сортування по заданому полю в порядку зростання та спадання значень;
+                var books2 = rep.GetReversed();
+                Console.WriteLine("List of Books ordered by Publication Year (Descending):");
+                foreach (Book book in books2)
+                {
+                    Console.WriteLine($"{book.Title} - {book.Author} - {book.PublicationYear}");
+                }
+
+                //Запит з використанням дій по модифікації записів
+                Console.Write("Enter the new firm name: ");
+                string newFirm = Console.ReadLine();
+                Console.Write("Enter the old firm name: ");
+                string oldFirm = Console.ReadLine();
+
+                var list = rep.Modification(oldFirm, newFirm);
+                Console.WriteLine("New list:");
+                foreach (var item in list)
+                {
+                    Console.WriteLine($"{item.Title}-{item.PublicationYear}-{item.PublisherAddress}-{item.BookstoreFirm}");
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Labs C# 2 kurs/Lab6 C#/Lab5/Lab5/SQL Tasks/E.sql
@@ -0,0 +1,3 @@
+﻿/*Змінити значення ціни, якщо вона зросте на 30%*/
+UPDATE Library
+SET Price = Library.Price * 1.3;
